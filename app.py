25fa6b3acde2ca4edc31652eb40608389e98da66
EvErToN <evertonflorenciodasilva@outlook.com>
2024-03-10 19:40:36 -0300

    criei o acesso ao banco de dados assim como o acesso aos bancos de documentos e nomes

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, jsonify
-
+import pymongo
 app = Flask(__name__)
+client = pymongo.MongoClient('mongodb+srv://herdeiros_aurora:herdeiros_aurora_001@cluster0.otugy2g.mongodb.net/')
+database = client['dev_ml_IAservice_DC_db']
+database = client['dev_ml_IA_NM']
 
 @app.route("/")
 def health():
